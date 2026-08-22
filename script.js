4d4ad0b476c6a6ba23d1120b4c3fd5d5c175a61e
Vipul Kumar Chaubey <vipulkumarchaubey1011@gmail.com>
2026-08-22 13:19:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
   const result=document.querySelector("#result")
   const button=document.querySelector("button")
   const bmi_form=document.querySelector("#bmi_form")
-const bmiType=document.querySelector(".bmiType")
-
-bmi_form.addEventListener("submit", function (e) {
-    e.preventDefault()  // protect from refreshing ... 
+ const bmiType=document.querySelector(".bmiType")
   // 1. Sirf HTML Element ko select karke save kara (taaki baad mein clear kar sake)
 const weightInput = document.querySelector("#weight"); 
 const heightInput = document.querySelector("#height");
+
+bmi_form.addEventListener("submit", function (e) {
+    e.preventDefault()  // protect from refreshing ... 
+
 
 // 2. Button click hone par us element ke andar ki value nikal li 
 const wt = parseInt(weightInput.value);
@@ -33,16 +34,16 @@ const ht = parseInt(heightInput.value);
         const BMI= ((wt*10000)/(ht*ht)).toFixed(2)
         result.innerText=`BMI is ${BMI}`
 
-        if(BMI<18.5){ 
+        if(BMI<=18.5){ 
             bmiType.innerHTML="Under Weight"
          }
-          else if(BMI>18.5&& BMI<24.9){ 
+          else if(BMI>18.5&& BMI<=24.9){ 
             bmiType.innerHTML="Healthy Weight"
          }
-          else if(BMI>25 && BMI<29.9){ 
+          else if(BMI>=25 && BMI<=29.9){ 
             bmiType.innerHTML="Over weight"
          }
-          else if(BMI>30){ 
+          else if(BMI>=30){ 
             bmiType.innerHTML="Obesity"
          }
       weightInput.value="";
